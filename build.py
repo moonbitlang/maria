a6d2ba75c9cae43f8daa4a03a497f876a60896a2
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-09-09 15:41:34 +0800

    fix: use libedit

--- build.py
+++ build.py
@@ -21,54 +21,6 @@ def extract(tar_path: str, extract_path: str):
         tar.extractall(path=extract_path)
 
 
-GPG_KEY = """-----BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v1.2.4 (Darwin)
-
-mQGiBEEOsGwRBACFa0A1oa71HSZLWxAx0svXzhOZNQZOzqHmSuGOG92jIpQpr8Dp
-vgRh40YpAwdcXb8QG1J5yGAKeevNE1zCFaA725vGSdHUyypHouV0xoWwukYO6qly
-yX+2BZU+okBUqoWQkoWxiYaCSfzB2Ln7pmdys1fJhcgBKf3VjWCjd2XJTwCgoFJO
-wyBFJdugjfwjSoRSwDOIMf0D/iQKqlWhIO1LGpMrGX0il0/x4zj0NAcSwAk7LaPZ
-bN4UPjn5pqGEHBlf1+xDDQCkAoZ/VqESGZragl4VqJfxBr29Ag0UDvNbUbXoxQsA
-Rdero1M8GiAIRc50hj7HXFoERwenbNDJL86GPLAQOTGOCa4W2o29nFfFjQrsrrYH
-zVtyA/9oyKvTeEMJ7NA3VJdWcmn7gOu0FxEmSNhSoV1T4vP21Wf7f5niCCRKQLNy
-Uy0wEApQi4tSysdz+AbgAc0b/bHYVzIf2uO2lIEZQNNt+3g2bmXgloWmW5fsm/di
-50Gm1l1Na63d3RZ00SeFQos6WEwLUHEB0yp6KXluXLLIZitEJLQaQ2hldCBSYW1l
-eSA8Y2hldEBjd3J1LmVkdT6IXgQTEQIAHgUCQQ6wbAIbAwYLCQgHAwIDFQIDAxYC
-AQIeAQIXgAAKCRC7WGnwZOp0q87NAJ99FEzFvDdYzqCczXF6KKXi7YN5OACfacDY
-soZcnnsy7EjBZL0zwGwb/sG5AQ0EQQ6wbxAEAJCukwDigRDPhAuI+lf+6P64lWan
-IFOXIndqhvU13cDbQ/Wt5LwPzm2QTvd7F+fcHOgZ8KOFScbDpjJaRqwIybMTcIN0
-B2pBLX/C10W1aY+cUrXZgXUGVISEMmpaP9v02auToo7XXVEHC+XLO9IU7/xaU98F
-L69l6/K4xeNSBRM/AAMHA/wNAmRBpcyK0+VggZ5esQaIP/LyolAm2qwcmrd3dZi+
-g24s7yjV0EUwvRP7xHRDQFgkAo6++QbuecU/J90lxrVnQwucZmfz9zgWDkT/MpfB
-/CNRSKLFjhYq2yHmHWT6vEjw9Ry/hF6Pc0oh1a62USdfaKAiim0nVxxQmPmiRvtC
-mYhJBBgRAgAJBQJBDrBvAhsMAAoJELtYafBk6nSr43AAn2ZZFQg8Gs/zUzvXMt7e
-vaFqVTzcAJ0cHtKpP1i/4H4R9+OsYeQdxxWxTQ==
-=2MjR
------END PGP PUBLIC KEY BLOCK-----
-"""
-
-
-def gpg_verify(sig_path: Path, tar_path: Path):
-    with tempfile.TemporaryDirectory() as tempdir:
-        key_path = Path(tempdir) / "gpgkey.asc"
-        with open(key_path, "w") as f:
-            f.write(GPG_KEY)
-        subprocess.run(
-            [
-                "gpg",
-                "--import",
-                key_path.resolve(),
-            ],
-            check=True,
-            cwd=tempdir,
-        )
-        subprocess.run(
-            ["gpg", "--verify", sig_path.resolve(), tar_path.resolve()],
-            check=True,
-            cwd=tempdir,
-        )
-
-
 VENDOR = Path("vendor")
 
 
@@ -92,11 +44,11 @@ def build_readline(src: Path):
 
 def is_readline_built():
     if platform.system() == "Windows":
-        lib_path = VENDOR / "lib" / "libreadline.lib"
+        lib_path = VENDOR / "lib" / "libedit.lib"
     elif platform.system() == "Darwin":
-        lib_path = VENDOR / "lib" / "libreadline.dylib"
+        lib_path = VENDOR / "lib" / "libedit.dylib"
     elif platform.system() == "Linux":
-        lib_path = VENDOR / "lib64" / "libreadline.so"
+        lib_path = VENDOR / "lib64" / "libedit.so"
     else:
         raise NotImplementedError(f"Unsupported platform: {platform.system()}")
     return lib_path.exists()
@@ -116,22 +68,17 @@ def main():
         text = sys.stdin.read()
         data = json.loads(text)
         env = data
-    tar_url = "ftp://ftp.gnu.org/gnu/readline/readline-8.3.tar.gz"
-    sig_url = tar_url + ".sig"
-    tar = VENDOR / "src" / "readline-8.3.tar.gz"
-    sig = VENDOR / "src" / "readline-8.3.tar.gz.sig"
+    tar_url = "https://thrysoee.dk/editline/libedit-20250104-3.1.tar.gz"
+    tar = VENDOR / "src" / "libedit-20250104-3.1.tar.gz"
     dst = VENDOR / "src"
     if not is_readline_built():
         if not dst.exists():
             dst.mkdir(parents=True, exist_ok=True)
         if not tar.exists():
             download(tar_url, tar)
-        if not sig.exists():
-            download(sig_url, sig)
-        gpg_verify(sig, tar)
-        shutil.rmtree(dst / "readline-8.3", ignore_errors=True)
+        shutil.rmtree(dst / "libedit-20250104-3.1", ignore_errors=True)
         extract(tar, dst)
-        build_readline(dst / "readline-8.3")
+        build_readline(dst / "libedit-20250104-3.1")
     cc = None
     if "CC" in env:
         cc = env["CC"]
@@ -152,15 +99,14 @@ def main():
     link_libs = []
     link_search_paths = []
     if platform.system() == "Windows":
-        link_libs.append(str((VENDOR / "lib").resolve() / "libhistory"))
-        link_libs.append(str((VENDOR / "lib").resolve() / "libreadline"))
+        link_libs.append(str((VENDOR / "lib").resolve() / "libedit"))
     elif platform.system() == "Darwin":
-        link_libs.append("history")
-        link_libs.append("readline")
+        link_libs.append("edit")
+        link_libs.append("ncurses")
         link_search_paths.append(str((VENDOR / "lib").resolve()))
     elif platform.system() == "Linux":
-        link_libs.append("history")
-        link_libs.append("readline")
+        link_libs.append("edit")
+        link_libs.append("ncurses")
         link_search_paths.append(str((VENDOR / "lib64").resolve()))
     else:
         raise NotImplementedError(f"Unsupported platform: {platform.system()}")
