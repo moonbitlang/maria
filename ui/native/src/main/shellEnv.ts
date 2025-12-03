68db085f36ae38a9f785da86655a9a6b2c808230
Bao Zhiyuan <baozhiyuan@idea.edu.cn>
2025-12-03 15:43:13 +0800

    tweak

--- ui/native/src/main/shellEnv.ts
+++ ui/native/src/main/shellEnv.ts
@@ -1,4 +1,4 @@
-// adopt from https://github.com/microsoft/vscode/blob/b03e656cfa33dfb86705f8631ff704c55d2ac660/src/vs/platform/shell/node/shellEnv.ts#
+// adopt from https://github.com/microsoft/vscode/blob/b03e656cfa33dfb86705f8631ff704c55d2ac660/src/vs/platform/shell/node/shellEnv.ts
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
